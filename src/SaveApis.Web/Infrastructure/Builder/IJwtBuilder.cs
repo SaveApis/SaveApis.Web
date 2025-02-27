1f1770feead9fae8f8a8badd8d4f4ff96e43ddab
SaveApis | Felix Hake <150133464+saveapisfelixhake@users.noreply.github.com>
2025-02-27 21:57:12 +0100

    feat(modules): JWT Module (#2)
    Author: Felix hake

---
+++ src/SaveApis.Web/Infrastructure/Builder/IJwtBuilder.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Claims;
+using SaveApis.Common.Infrastructure.Builder;
+
+namespace SaveApis.Web.Infrastructure.Builder;
+
+public interface IJwtBuilder : IBuilder<string>
+{
+    IJwtBuilder AddClaim(string type, string value);
+    IJwtBuilder AddClaim(Claim claim);
+    IJwtBuilder AddClaims(params Claim[] claims);
+}
