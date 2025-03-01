a23d257b5f9596fb0e5a4931999ef64a3005f63f
SaveApis | Felix Hake <felix.hake@saveapis.com>
2025-03-01 15:03:28 +0100

    feat(modules): Swagger Module
    Author: Felix hake

---
+++ src/SaveApis.Web/Application/DI/SwaggerModule.cs
@@ -0,0 +1,32 @@
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SaveApis.Web.Infrastructure.DI;
+
+namespace SaveApis.Web.Application.DI;
+
+public class SwaggerModule : BaseWebModule
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        var collection = new ServiceCollection();
+
+        collection.AddEndpointsApiExplorer();
+        collection.AddSwaggerGen();
+
+        builder.Populate(collection);
+    }
+
+    protected override void PostAuthentication(WebApplication application)
+    {
+        if (application.Environment.IsProduction())
+        {
+            return;
+        }
+
+        application.UseSwagger();
+        application.UseSwaggerUI();
+    }
+}
