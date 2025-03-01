78004d63737450279bcd9d100427f3ed335f473d
SaveApis | Felix Hake <felix.hake@saveapis.com>
2025-03-01 11:39:54 +0100

    feat(modules): REST Module
    Author: Felix hake

---
+++ example/Example/Domains/Jwt/Application/Backend/JwtController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SaveApis.Web.Infrastructure.Builder;
+
+namespace Example.Domains.Jwt.Application.Backend;
+
+[ApiController]
+[Route("api/jwt")]
+public class JwtController(IJwtBuilder builder) : ControllerBase
+{
+    [HttpPost("login")]
+    public IActionResult Login()
+    {
+        var token = builder.Build();
+
+        return Ok(token);
+    }
+
+    [Authorize]
+    [HttpGet("authorized")]
+    public IActionResult Authorized()
+    {
+        return Ok("Authorized");
+    }
+}
