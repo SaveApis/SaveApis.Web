a8a8deda3dbdb8a758a2d9c8ee57730f2a0c0cf4
SaveApis | Felix Hake <150133464+saveapisfelixhake@users.noreply.github.com>
2025-03-01 11:55:57 +0100

    feat(modules): CorrelateWeb module (#5)
    Author: Felix hake

---
+++ src/SaveApis.Web/Application/DI/CorrelateWebModule.cs
@@ -0,0 +1,26 @@
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Correlate.AspNetCore;
+using Correlate.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using SaveApis.Web.Infrastructure.DI;
+
+namespace SaveApis.Web.Application.DI;
+
+public class CorrelateWebModule : BaseWebModule
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        var collection = new ServiceCollection();
+
+        collection.AddCorrelate(options => options.IncludeInResponse = true);
+
+        builder.Populate(collection);
+    }
+
+    protected override void PreAuthentication(WebApplication application)
+    {
+        application.UseCorrelate();
+    }
+}
